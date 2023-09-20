584499ccb7c0cf0c73417a7f0b5abd6c7c3bd1ea
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-09-20 11:58:58 -0700

    Update the demo.

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # llama-index
+
 A simple Streamlit web app for using [LlamaIndex](https://github.com/jerryjliu/llama_index), an interface to connect LLM’s with external data.
 
 For a step-by-step guide, see [this](https://alphasec.io/query-your-own-documents-with-llamaindex-and-langchain/) post. To deploy on [Railway](https://railway.app/?referralCode=alphasec), click the button below.
 
 [![Deploy on Railway](https://railway.app/button.svg)](https://railway.app/new/template/GpZ0J4?referralCode=alphasec)
+
+## Quick start
+
+1. `pip install -r requirements`
+1. `pip install streamlit`
+1. `streamlit run index.py`
--- index.py
+++ index.py
@@ -1,36 +1,38 @@
 import os, streamlit as st
 
-# Uncomment to specify your OpenAI API key here (local testing only, not in production!), or add corresponding environment variable (recommended)
-# os.environ['OPENAI_API_KEY']= ""
+from llama_index import VectorStoreIndex, SimpleDirectoryReader, ServiceContext
+from llama_index.llms import LlamaCPP
+from llama_index.llms.llama_utils import messages_to_prompt, completion_to_prompt
 
-from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader, LLMPredictor, PromptHelper, ServiceContext
-from langchain.llms.openai import OpenAI
+st.title("Ask Aria")
+query = st.text_input(
+    "What would you like to ask? (source: data/paul_graham_essay.txt)", ""
+)
 
-# Define a simple Streamlit app
-st.title("Ask Llama")
-query = st.text_input("What would you like to ask? (source: data/paul_graham_essay.txt)", "")
-
-# If the 'Submit' button is clicked
 if st.button("Submit"):
     if not query.strip():
         st.error(f"Please provide the search query.")
     else:
         try:
-            # This example uses text-davinci-003 by default; feel free to change if desired
-            llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003"))
-
-            # Configure prompt parameters and initialise helper
-            max_input_size = 4096
-            num_output = 256
-            max_chunk_overlap = 20
-
-            prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
+            llm = LlamaCPP(
+                model_url="https://huggingface.co/TheBloke/Llama-2-13B-chat-GGUF/resolve/main/llama-2-13b-chat.Q4_0.gguf",
+                temperature=0.1,
+                max_new_tokens=256,
+                context_window=3900,
+                generate_kwargs={},
+                model_kwargs={"n_gpu_layers": 1},
+                messages_to_prompt=messages_to_prompt,
+                completion_to_prompt=completion_to_prompt,
+                verbose=True,
+            )
 
             # Load documents from the 'data' directory
-            documents = SimpleDirectoryReader('data').load_data()
-            service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
-            index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
-            
+            documents = SimpleDirectoryReader("data").load_data()
+            service_context = ServiceContext.from_defaults(llm=llm)
+            index = VectorStoreIndex.from_documents(
+                documents, service_context=service_context
+            )
+
             response = index.query(query)
             st.success(response)
         except Exception as e:
